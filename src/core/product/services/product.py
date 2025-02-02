28ef60057138bd58d917738d23d7737d284cd579
Maxim <yrev2011@gmail.com>
2025-02-02 14:02:51 +0700

    Add product service

--- src/core/product/services/product.py
+++ src/core/product/services/product.py
@@ -1,3 +1,4 @@
+from src.core.product.dto.product import UpdateProductDTO
 from src.core.product.entities.product import Product
 from src.core.product.interfaces.repositories.product import IProductRepository
 
@@ -9,5 +10,8 @@ class ProductService:
     async def create_product(self, product: Product) -> None:
         await self._repository.add_product_on_db(product)
 
-    async def update_product(self):
-        ...
+    async def update_product(self, product_id: int, update_product: UpdateProductDTO) -> None:
+        await self._repository.update_product_on_db(product_id, update_product)
+
+    async def delete_product(self, product_id: int) -> None:
+        await self._repository.delete_product_from_db(product_id)
